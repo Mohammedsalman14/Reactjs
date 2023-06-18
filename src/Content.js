c41d4bd53cbc26c5aa8f026258e2af5ca9d03af8
Mohammedsalman14 <mohamedsalman140@gmail.com>
2023-06-18 16:34:37 +0530

    useState Updated

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import './Content.css';
+import { useState } from 'react';
+
 const Content = () => {
-    function handlenamesChange() {
-        const names = ["Earn", "Grow", "Give"];
-        const int = Math.floor(Math.random() * 3);
-        return names[int];
+    // function handlenamesChange() {
+    //     const names = ["Earn", "Grow", "Give"];
+    //     const int = Math.floor(Math.random() * 3);
+    //     return names[int];
+    // }
+
+
+    const [count2, updatecount] = useState(1);
+
+    function increment() {
+        updatecount(count2 + 1)
+    }
+    function decrement() {
+        updatecount(count2 - 1);
+    }
+
+    const [count, setCount] = useState(99)
+    function incrementFunction() {
+        setCount((preCount) => { return preCount + 1 });
+    }
+    function decrementFunction() {
+        setCount((preCount) => { return preCount - 1 });
+    }
+    function handleClick(e) {
+        console.log(e.target.innerText);
     }
     return (
-       <p>  Lets {handlenamesChange()} Money</p>
+        <div>
+            <button onClick={() => decrement()}>--</button>
+            <span>{count2}</span>
+            <button onClick={() => increment()}>++</button>
+            <p>  Lets Earn Money</p> <br />
+            <button onClick={(e) => handleClick(e)}> Click </button>
+            <button onClick={() => decrementFunction()}>-</button>
+            <span>{count}</span>
+            <button onClick={() => incrementFunction()}>+</button>
+        </div>
+
     )
 }
 
